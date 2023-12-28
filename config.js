6831606556364e7ad3044ee0944bb2d8eb064ccc
RanTech2023 <138270707+RanTech2023@users.noreply.github.com>
2023-12-29 00:03:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,10 @@ const chalk = require('chalk');
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = {
       BASE_URL: "https://queen-api-production.up.railway.app", //Queen-AI Base API Url
-      SESSION_ID: process.env.SESSION_ID || "PASTE HERE", //enter your session id here
-      OWNER_NUMBER: process.env.OWNER_NUMBER || "9277XXXXXX", //Owner whatsapp number here
-      OWNER_NAME: process.env.OWNER_NAME || "DarkWinzo", //Queen-AI Owner Name here
+      SESSION_ID: process.env.SESSION_ID || "QUEEN-AI;;;PrizeKeeping
+", //enter your session id here
+      OWNER_NUMBER: process.env.OWNER_NUMBER || "94705281879", //Owner whatsapp number here
+      OWNER_NAME: process.env.OWNER_NAME || "Tilan Ransara", //Queen-AI Owner Name here
       PREFIX: ".", //Don't Change This..
       LANGUAGE: process.env.LANGUAGE || "SI" //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu /FR = French
 };
